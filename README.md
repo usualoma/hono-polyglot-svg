625e4bc9d431c1c56c4c5bc4a56a383c7149cdf0
Taku Amano <taku@taaas.jp>
2024-12-01 22:20:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+npm install
+npm run dev
+```
+
+```
+open http://localhost:3000/static/hono.svg?eval=1
+```
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import type { Context } from "hono";
+import { Hono } from "hono";
+import { resolveCallback, HtmlEscapedCallbackPhase } from "hono/utils/html";
+import { some } from "hono/combine";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+import { writeFile } from "fs/promises";
+import { build } from "esbuild";
+
+const app = new Hono();
+
+app.get(
+  "/static/*",
+  some(
+    (c: Context) => !c.req.query("eval"),
+    async (c, next) => {
+      await next();
+
+      const source = await c.res.clone().text();
+      const result = await build({
+        stdin: {
+          contents: source,
+          loader: "tsx",
+          resolveDir: ".",
+        },
+        write: false,
+        bundle: true,
+        format: "esm",
+        jsx: "automatic",
+        jsxImportSource: "hono/jsx",
+      });
+      const jsxNode = eval(result.outputFiles[0].text);
+      const out = await resolveCallback(
+        jsxNode.toString(),
+        HtmlEscapedCallbackPhase.Stringify,
+        false,
+        {}
+      );
+
+      const outPath = new URL(c.req.url).pathname.replace(
+        "/static/",
+        "./static/"
+      );
+      await writeFile(outPath, out);
+
+      c.res = new Response(out, {
+        headers: {
+          "content-type": "image/svg+xml",
+          "content-length": out.length.toString(),
+        },
+      });
+    }
+  ),
+  serveStatic({ root: "./" })
+);
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
